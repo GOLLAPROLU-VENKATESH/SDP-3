0705033fee979bc89b07c17d106e350c76b8243a
GOLLAPROLU-VENKATESH <63569956+GOLLAPROLU-VENKATESH@users.noreply.github.com>
2021-12-31 08:59:50 +0530

    Update StoreRepository.java

--- src/main/java/com/sdp3/SDP3/repository/StoreRepository.java
+++ src/main/java/com/sdp3/SDP3/repository/StoreRepository.java
@@ -8,9 +8,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface StoreRepository extends JpaRepository<Store,Long> {
 
-    @Query(value = "SELECT * FROM Store  WHERE user_id = ?1",nativeQuery = true)
+    @Query(value = "SELECT * FROM store  WHERE user_id = ?1",nativeQuery = true)
     Store findByUser_id(Long userId);
 
-    @Query(value = "SELECT * FROM Store  WHERE store_id = ?1",nativeQuery = true)
+    @Query(value = "SELECT * FROM store  WHERE store_id = ?1",nativeQuery = true)
     Store getStoreByStoreId(Long sid);
 }
